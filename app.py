063318999a3a6aaf3124c3e2c619080af73b5a4e
patrickhsbr <patrick.heuschober@gmail.com>
2023-05-30 23:39:07 +0200

    final update

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+# import all required packages listed in the requirements.txt
 from flask import Flask, render_template, request
 import yfinance as yf
 import plotly.graph_objs as go
-import pandas as pd
 
 app = Flask(__name__)
 
+# path of text file containing all the tickers you want to show in the pull down select on the home 
 tickers_file_path = '/Users\patri\Sandbox\Stock_Ticker/tickers.txt'
 
-# Load ticker symbols and company names from tickers.txt
+# function to get the ticker symbols and company names from tickers.txt to show them in the pull down select
 def load_tickers():
 
     ticker_data = {}
@@ -17,39 +18,39 @@ def load_tickers():
             ticker_data[ticker] = company
     return ticker_data
 
-# home page with user input form
+# home page on which the user can select a stock picker in the the input form
 @app.route("/")
 def home():
     ticker_data = load_tickers()
     return render_template("home.html", ticker_data=ticker_data)
 
-# Error page
+# error page in case the user requests leads to an empty response from yahoo finance
 @app.route("/error")
 def error():
     return render_template("error.html")
 
-# result page with chart
+# response page visualizing the data retrieved from yahoo finance in a candlestick chart
 @app.route("/response", methods=["POST"])
 def response():
     ticker_data = load_tickers()
 
-    # processing user input, asking for a specified stock code/ ticker symbol
+    # send a post request with the user input which is a yahoo finance ticker symbol
     if request.method == "POST":
         ticker_input = request.form["ticker"]
         processed_input = ticker_input.upper()
 
-        # Retrieve data from Yahoo Finance with specified stock code/ ticker symbol
+        # retrieve data from yahoo finance with specified ticker symbol set as variable processed_input
         data = yf.download(processed_input)
 
-        # error handling for user inputs with empty API response
+        # redirect the user to the error page in case the api response based on the stock ticker is empty
         if data.empty:
             return redirect("/error")
 
-        # retrieving company information "longName" for specified stock code/ ticker symbol
+        # retrieving the company information "longName" for the sent ticker symbol and set as variable ticker_long_name
         yticker = yf.Ticker(processed_input)
-        company_name = yticker.info["longName"]
+        ticker_long_name = yticker.info["longName"]
 
-        # Create a new Plotly figure with a candlestick trace
+        # specify the candlestick chart visualizing the retrieved data using the package plotly
         fig = go.Figure(data=[go.Candlestick(
             x=data.index,
             open=data['Open'],
@@ -60,16 +61,18 @@ def response():
             decreasing=dict(line=dict(color='#ff6347'))
         )])
 
-        # Set the chart layout with some style and a range slider
+        # specify the style of the chart
         fig.update_layout(
+            # add the title of the requested stock ticker by pasting the string of the variable ticker_long_name 
             title={
-                'text': company_name,
+                'text': ticker_long_name,
                 'font': {
                     'size': 22,
                     'color': '#B3B3B3'
                 }
 
             },
+            # specify buttons for the user and map different time periods to them 
             xaxis=dict(
                 title="Date",
                 tickformat='%d/%m/%Y',
@@ -85,30 +88,30 @@ def response():
                     ]),
                     bgcolor="#27374D"
                 ),
+                # add a rangeslider
                 rangeslider=dict(
                     visible=True,
                     thickness=0.1
                 ),
                 type="date"
             ),
+            # add a axis descritpion hardcoded
             yaxis=dict(
                 title="Price (USD)",
                 tickprefix="$",
                 showgrid=False
             ),
-            margin=dict(l=60, r=60, t=100, b=40),
+            # define the size of the chart
+            margin=dict(l=20, r=60, t=100, b=40),
             plot_bgcolor='#27374D',
             paper_bgcolor='#27374D',
             font=dict(color='#B3B3B3')
         )
 
-        # Update the y-axis range dynamically based on the visible data
+        # add a dynamic range slide to the x axis based on the visible data
         fig.update_xaxes(rangeslider=dict(visible=True))
         fig.update_layout(xaxis_rangeslider_visible=False)
 
-        # Convert the Plotly figure to HTML and pass it to the template
+        # convert the Plotly figure to htmal and populate it on the response.hmtl page
         chart = fig.to_html(full_html=False)
         return render_template("response.html", chart=chart)
-
-if __name__ == "__main__":
-    app.run()
